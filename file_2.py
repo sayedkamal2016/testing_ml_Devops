ba20f886cccddbc6371dc8c63a126a8f0032b311
Elsayed <sayed2006_2007@yahoo.com>
2021-05-16 21:59:44 +0200

    commiting test file 2

---
+++ file_2.py
@@ -0,0 +1,15 @@
+"""
+This is Sayed Kamal, and I am testing git. 
+
+"""
+
+def print_names():
+    name = input(" Insert your name plz> ")
+    name = str(name)
+
+    print("Welcome, {}".format(name))
+
+
+if __name__ == "__main__":
+
+    print_names()
